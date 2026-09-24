a945bb262032bc7d8b8addfc9fbde51c6d8565d6
Andriy <andrijbuluk@gmail.com>
2026-09-24 19:40:16 +0200

    Update name for async function

--- src/database/session_postgresql.py
+++ src/database/session_postgresql.py
@@ -53,7 +53,7 @@ async def get_postgresql_db_contextmanager() -> AsyncGenerator[AsyncSession, Non
         yield session
 
 
-async def reset_postgresql_database():
-    async with postgresql_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+async def reset_database():
+  async with postgresql_engine.begin() as conn:
+    await conn.run_sync(Base.metadata.drop_all)
+    await conn.run_sync(Base.metadata.create_all)
