c5762931de84ccbd9d1956cd4b1715c0f4cb3d59
Andriy <andrijbuluk@gmail.com>
2026-09-26 16:37:25 +0200

    Fix: back to sqlite

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,10 +1,10 @@
 import os
 
-from database.session_postgresql import reset_database
 from database.models import (
     Base,
     MovieModel
 )
+from database.session_sqlite import reset_sqlite_database as reset_database
 
 environment = os.getenv("ENVIRONMENT", "developing")
 
